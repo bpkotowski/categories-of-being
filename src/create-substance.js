5a8ea17914db8514a8509e87461aa7a3f7e95444
Peter Kotowski <bighoopee@gmail.com>
2019-02-10 20:46:48 -0800

    captured data in local storage

--- src/create-substance.js
+++ src/create-substance.js
@@ -1,4 +1,3 @@
-// declare variable nodes from user form
 const inputNode = document.getElementById('input');
 const essenceNode = document.getElementById('essence');
 const quantityNode = document.getElementById('quantity');
@@ -10,12 +9,37 @@ const postureNode = document.getElementById('posture');
 const havingNode = document.getElementById('having');
 const doingNode = document.getElementById('doing');
 const affectionNode = document.getElementById('affection');
-// variable to send to local storage
-const categories = 
+let categories = [];
 
-console.log(postureNode);
+const categoriesJSON = window.localStorage.getItem('categories');
+console.log(categoriesJSON);
 
 
+inputNode.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const category = {
+        essence: essenceNode.value,
+        quantity: quantityNode.value,
+        qualification: qualificationNode.value,
+        relative: relativeNode.value,
+        where: whenNode.value,
+        when: whenNode.value,
+        posture: postureNode.value, 
+        doing: doingNode.value,
+        affection: affectionNode.value,
+    
+    
+    };
+    
+    categories.push(category);
+    
+    const categoriesJSON = JSON.stringify(categories);
+    window.localStorage.setItem('categories', categoriesJSON);
+   
+})
+
+console.log('hello');
 
 
 
